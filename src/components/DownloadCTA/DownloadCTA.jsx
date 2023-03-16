9bd39c28f22604f7e1e701b37792c33e6a9b807d
Muhammad Talha <talhahanfsd@gmail.com>
2023-03-16 18:26:17 +0500

    Change Z index of Components

--- src/components/DownloadCTA/DownloadCTA.jsx
+++ src/components/DownloadCTA/DownloadCTA.jsx
@@ -5,8 +5,8 @@ const DownloadCTA = () => {
   return (
     <div className="xl:max-w-[1440px] px-[1.5rem] md:px-[3rem] lg:px-[6rem] mx-auto w-full">
     <div className=' bg-blue relative p-8 md:p-10 lg:p-20 flex flex-col flex-wrap content-center rounded-[35px] md:rounded-[50px] lg:rounded-[50px] overflow-hidden'>
-      <div className="text-2xl md:text-3xl lg:text-4xl pb-6 md:pb-8 lg:pb-12 font-semibold text-center text-white max-w-[756px] z-[10]">Download an app now and the various benefits you will get immediately</div>
-      <div className="mx-auto flex md:flex-row flex-col content-center z-[10]">
+      <div className="text-2xl md:text-3xl lg:text-4xl pb-6 md:pb-8 lg:pb-12 font-semibold text-center text-white max-w-[756px] z-[1]">Download an app now and the various benefits you will get immediately</div>
+      <div className="mx-auto flex md:flex-row flex-col content-center z-[1]">
             <button
               className={`transition-all m-2 ease-in duration-500 px-10 py-3 border-2 text-[16px] rounded-full cursor-pointer hover:duration-500 text-blue bg-white inline-block`}
             >
